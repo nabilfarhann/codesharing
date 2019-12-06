2a61826ada7fe7dac60efdff02890206e8cf737d
Nabil Farhan <nabilfarhan@Nabils-MacBook-Air.local>
2019-12-06 22:48:31 +0800

    changes applied

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ module.exports = {
             pass: '7891235nA'
         }
     },
-    dbConnstring: 'mongodb://admin:1234@sharingcode-shard-00-00-fmn60.mongodb.net:27017,sharingcode-shard-00-01-fmn60.mongodb.net:27017,sharingcode-shard-00-02-fmn60.mongodb.net:27017/test?ssl=true&replicaSet=sharingcode-shard-0&authSource=admin&retryWrites=true&w=majority',
+    dbConnstring: 'mongodb://admin:1234@sharingcode-shard-00-00-fmn60.mongodb.net:27017,sharingcode-shard-00-01-fmn60.mongodb.net:27017,sharingcode-shard-00-02-fmn60.mongodb.net:27017/codesharing?ssl=true&replicaSet=sharingcode-shard-0&authSource=admin&retryWrites=true&w=majority',
     sessionKey: 'HaloCodeSharing'
 }
