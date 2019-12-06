f754af47db62c0759664fb0f66dc8743796a6af9
Nabil Farhan <nabilfarhan@Nabils-MacBook-Air.local>
2019-12-06 22:25:04 +0800

    my first commit

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ module.exports = {
             pass: '7891235nA'
         }
     },
-    dbConnstring: 'mongodb+srv://admin:<123>@cluster0-fmn60.gcp.mongodb.net/test?retryWrites=true&w=majority',
+    dbConnstring: 'mongodb+srv://admin:<1234>@cluster0-fmn60.gcp.mongodb.net/test?retryWrites=true&w=majority',
     sessionKey: 'HaloCodeSharing'
 }
